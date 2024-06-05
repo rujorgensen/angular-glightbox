59771df66deaf8223663342b65e2e973fc7735fa
rj <rj@oxyguard.dk>
2024-06-05 15:40:04 +0200

    Move folder. SWitch to new Angular template syntax for if and for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './contents/gallery.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
