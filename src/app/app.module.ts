469fd91e9863ea786c7b3d43002e2335f698eda8
rj <rj@oxyguard.dk>
2024-06-05 14:43:30 +0200

    removed router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Step1Component } from './contents/step1/step1/step1.component';
+import { Step1Component } from './contents/step1.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +10,6 @@ import { Step1Component } from './contents/step1/step1/step1.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
