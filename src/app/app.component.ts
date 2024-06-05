e22e316b70fe559b7a9359330722640fd18641e6
rj <rj@oxyguard.dk>
2024-06-05 15:56:08 +0200

    Removed unused styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-gallery [gallery]="gallery" />',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
